4ce6090d353cc3daba928aaf01fdffdb610f4a72
julio-nt <julio.nt753@gmail.com>
2026-04-30 00:15:03 -0300

    fix: z index da navbar

--- src/modules/_layout/components/Navbar.tsx
+++ src/modules/_layout/components/Navbar.tsx
@@ -32,7 +32,7 @@ const Navbar = () => {
   return (
     <>
       <nav
-        className={`fixed bg-background flex items-center justify-between gap-4 w-full h-16 border-b px-4 ${open ? 'md:pl-68' : ''} transition-all`}
+        className={`fixed z-[9] bg-background flex items-center justify-between gap-4 w-full h-16 border-b px-4 ${open ? 'md:pl-68' : ''} transition-all`}
       >
         <SidebarTrigger />
 
